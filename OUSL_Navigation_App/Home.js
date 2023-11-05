e5f4e08ea662cfc723cd92fb87e3e088d7899111
Thizh <thisitha2001@gmail.com>
2023-11-05 19:03:42 +0530

    new changes

---
+++ OUSL_Navigation_App/Home.js
@@ -0,0 +1,78 @@
+import React, { useRef, useState } from 'react';
+import { Animated, Image, TouchableOpacity, StyleSheet, View } from 'react-native';
+import { PanGestureHandler, PinchGestureHandler } from 'react-native-gesture-handler';
+
+const MapScreen = () => {
+  const [scale, setScale] = useState(1);
+  const panRef = useRef();
+  const pinchRef = useRef();
+  const baseScale = useRef(new Animated.Value(1)).current;
+  const pinchScale = useRef(new Animated.Value(1)).current;
+  let lastScale = 1;
+
+  const onPinchGestureEvent = Animated.event(
+    [{ nativeEvent: { scale: pinchScale } }],
+    { useNativeDriver: true }
+  );
+
+  const onZoomIn = () => {
+    setScale(scale * 1.1);
+  };
+
+  const onZoomOut = () => {
+    setScale(scale * 0.9);
+  };
+
+  // Don't forget to handle the state changes and animations for panning as well.
+
+  return (
+    <View style={styles.container}>
+      <PanGestureHandler
+        ref={panRef}
+        onGestureEvent={/* ... */}>
+        <PinchGestureHandler
+          ref={pinchRef}
+          onGestureEvent={onPinchGestureEvent}
+          onHandlerStateChange={/* ... */}>
+          <Animated.Image
+            source={require('./path-to-your-map-image.png')}
+            style={[styles.map, { transform: [{ scale: Animated.multiply(baseScale, pinchScale) }] }]}
+            resizeMode="contain"
+          />
+        </PinchGestureHandler>
+      </PanGestureHandler>
+      <TouchableOpacity onPress={onZoomIn} style={styles.zoomIn}>
+        {/* Add Zoom In Icon */}
+      </TouchableOpacity>
+      <TouchableOpacity onPress={onZoomOut} style={styles.zoomOut}>
+        {/* Add Zoom Out Icon */}
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  map: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  zoomIn: {
+    position: 'absolute',
+    right: 10,
+    bottom: 60,
+    // Add styles for the button
+  },
+  zoomOut: {
+    position: 'absolute',
+    right: 10,
+    bottom: 10,
+    // Add styles for the button
+  }
+});
+
+export default MapScreen;
--- OUSL_Navigation_App/OpenScreen.js
+++ OUSL_Navigation_App/OpenScreen.js
@@ -1,11 +1,25 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
+import React, { useEffect } from 'react'; // import useEffect
+import { useNavigation } from '@react-navigation/native'; // import useNavigation
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function OpenScreen() {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      // Navigate to 'Home' after 5 seconds
+      navigation.navigate('Home');
+    }, 5000); // 5000 ms = 5 seconds
+
+    // Clear the timer when the component unmounts
+    return () => clearTimeout(timer);
+  }, [navigation]);
+
   return (
     <View style={styles.container}>
       <Image source={require('./assets/Logo-OUSL.png')} style={styles.img} />
-      <Text style={{fontWeight: 700, margin: 5}}>OPEN UNIVERSITY OF SRI LANKA</Text>
+      <Text style={styles.text}>OPEN UNIVERSITY OF SRI LANKA</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,9 +32,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-
   img: {
     height: 148,
     width: 104,
+  },
+  text: { // Updated style for text
+    fontWeight: 'bold', // Use 'bold' instead of 700
+    margin: 5,
   }
 });
