fed23986da6c3e9568d772c8578f45441c991901
Ayeshan Weerasuriya <98686381+ayeshanweerasuriya@users.noreply.github.com>
2023-11-23 19:42:38 +0530

    With Dark Mode (Only For settings Screen )

--- OUSL-map/Settings.js
+++ OUSL-map/Settings.js
@@ -11,6 +11,7 @@ import Icon from "react-native-vector-icons/FontAwesome";
 
 const CustomSwitch = () => {
   const [isEnabled, setIsEnabled] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
   const toggleAnim = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
@@ -24,6 +25,7 @@ const CustomSwitch = () => {
 
   const toggleSwitch = () => {
     setIsEnabled((previousState) => !previousState);
+    setIsDarkMode((previousState) => !previousState);
   };
 
   const togglePosition = toggleAnim.interpolate({
@@ -32,7 +34,7 @@ const CustomSwitch = () => {
   });
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, isDarkMode && styles.darkTheme]}>
       <View style={styles.darkModeContainer}>
         <Text style={styles.OptionText}>Dark Mode</Text>
         <TouchableOpacity
@@ -55,22 +57,38 @@ const CustomSwitch = () => {
       </View>
       <View style={styles.otherContainers}>
         <TouchableOpacity style={styles.otherContainersAlign}>
-          <Text style={styles.OptionText}>About Us</Text>
-          <Icon name="chevron-right" size={20} color="#000000" />
+          <Text style={[styles.OptionText, isDarkMode && styles.darkText]}>
+            About Us
+          </Text>
+          <Icon
+            name="chevron-right"
+            size={20}
+            color={isDarkMode ? "#FFFFFF" : "#000000"}
+          />
         </TouchableOpacity>
       </View>
       <View style={styles.otherContainers}>
         <TouchableOpacity style={styles.otherContainersAlign}>
-          <Text style={styles.OptionText}>Privacy Policy</Text>
-          <Icon name="chevron-right" size={20} color="#000000" />
+          <Text style={[styles.OptionText, isDarkMode && styles.darkText]}>
+            Privacy Policy
+          </Text>
+          <Icon
+            name="chevron-right"
+            size={20}
+            color={isDarkMode ? "#FFFFFF" : "#000000"}
+          />
         </TouchableOpacity>
       </View>
 
       <TouchableOpacity style={styles.bug}>
-        <Icon name="bug" size={15} color="#000000" />
-        <Text style={styles.bugText}>Report a Bug</Text>
+        <Icon name="bug" size={15} color={isDarkMode ? "#FFFFFF" : "#000000"} />
+        <Text style={[styles.bugText, isDarkMode && styles.darkText]}>
+          Report a Bug
+        </Text>
       </TouchableOpacity>
-      <Text style={styles.versionNum}>V 0.1</Text>
+      <Text style={[styles.versionNum, isDarkMode && styles.darkText]}>
+        V 0.1
+      </Text>
     </View>
   );
 };
@@ -78,8 +96,12 @@ const CustomSwitch = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 150,
-    margin: 20,
+    paddingTop: 150,
+    padding: 20,
+  },
+
+  darkTheme: {
+    backgroundColor: "#1E1E1E",
   },
 
   darkModeContainer: {
@@ -95,6 +117,10 @@ const styles = StyleSheet.create({
   OptionText: {
     fontWeight: "bold",
     fontSize: 15,
+  },
+
+  darkText: {
+    color: "#FFFFFF",
   },
 
   switchStyle: {
@@ -125,8 +151,6 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
   },
   otherContainers: {
-    // backgroundColor: "#F4F4F4",
-    backgroundColor: "#ffffff",
     padding: 20,
     borderRadius: 10,
   },
