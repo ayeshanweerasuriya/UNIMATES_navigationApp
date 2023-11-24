7c9aa6f2f6013ff993ee613e21ef8ae9b6cd2a5b
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2023-11-25 02:00:27 +0530

    Change color of BottomToolbar and Add Header to Settings.js

--- OUSL-map/BottomToolbar.js
+++ OUSL-map/BottomToolbar.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
-import IconAnt from 'react-native-vector-icons/AntDesign';
-import IconEnt from 'react-native-vector-icons/Entypo';
-import IconFea from 'react-native-vector-icons/Feather';
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
+import IconAnt from "react-native-vector-icons/AntDesign";
+import IconEnt from "react-native-vector-icons/Entypo";
+import IconFea from "react-native-vector-icons/Feather";
 
 const BottomToolbar = ({ navigation }) => {
   return (
     <View style={styles.toolbar}>
-      <TouchableOpacity style={[styles.tools, styles.shadow]} onPress={() => navigation.navigate('Search')}>
+      <TouchableOpacity
+        style={[styles.tools, styles.shadow]}
+        onPress={() => navigation.navigate("Search")}
+      >
         <IconAnt name="search1" size={25} color="#fff" />
       </TouchableOpacity>
       <TouchableOpacity style={styles.tools}>
@@ -17,7 +26,12 @@ const BottomToolbar = ({ navigation }) => {
         <IconEnt name="save" size={25} color="#fff" />
       </TouchableOpacity>
       <TouchableOpacity style={styles.tools}>
-        <IconFea name="settings" size={25} color="#fff" onPress={() => navigation.navigate('Settings')} />
+        <IconFea
+          name="settings"
+          size={25}
+          color="#fff"
+          onPress={() => navigation.navigate("Settings")}
+        />
       </TouchableOpacity>
     </View>
   );
@@ -26,27 +40,27 @@ const BottomToolbar = ({ navigation }) => {
 const styles = StyleSheet.create({
   toolbar: {
     bottom: 0,
-    position: 'absolute',
+    position: "absolute",
     width: Dimensions.get("window").width,
     height: 85,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    backgroundColor: '#BA7815',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#FFA500",
+    paddingBottom: 20,
   },
   tools: {
     marginLeft: 25,
     marginRight: 25,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   shadow: {
-    shadowColor: 'rgba(0, 0, 0, 0.25)',
+    shadowColor: "rgba(0, 0, 0, 0.25)",
     shadowOffset: { width: 0, height: 0 },
     shadowOpacity: 1,
     shadowRadius: 10,
     elevation: 2,
   },
 });
-
 
 export default BottomToolbar;
--- OUSL-map/Settings.js
+++ OUSL-map/Settings.js
@@ -9,6 +9,7 @@ import {
 } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { useTheme } from "./ThemeContext";
+import CustomHeader from "./Header";
 
 const Settings = ({ navigation }) => {
   const { isDarkMode, toggleDarkMode } = useTheme();
@@ -36,75 +37,82 @@ const Settings = ({ navigation }) => {
   });
 
   return (
-    <View style={[styles.container, isDarkMode && styles.darkTheme]}>
-      <View style={styles.darkModeContainer}>
-        <Text style={styles.OptionText}>Dark Mode</Text>
-        <TouchableOpacity
-          activeOpacity={0.8}
-          onPress={toggleSwitch}
-          style={[
-            styles.switchStyle,
-            { backgroundColor: isEnabled ? "#FFA500" : "#767577" },
-          ]}
-        >
-          <View style={styles.toggleContainer}>
-            <Animated.View
-              style={[
-                styles.toggleButton,
-                { transform: [{ translateX: togglePosition }] },
-              ]}
+    <>
+      <CustomHeader title="Settings" navigation={navigation} />
+      <View style={[styles.container, isDarkMode && styles.darkTheme]}>
+        <View style={styles.darkModeContainer}>
+          <Text style={styles.OptionText}>Dark Mode</Text>
+          <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={toggleSwitch}
+            style={[
+              styles.switchStyle,
+              { backgroundColor: isEnabled ? "#FFA500" : "#767577" },
+            ]}
+          >
+            <View style={styles.toggleContainer}>
+              <Animated.View
+                style={[
+                  styles.toggleButton,
+                  { transform: [{ translateX: togglePosition }] },
+                ]}
+              />
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.otherContainers}>
+          <TouchableOpacity
+            style={styles.otherContainersAlign}
+            onPress={() => navigation.navigate("AboutUs")}
+          >
+            <Text style={[styles.OptionText, isDarkMode && styles.darkText]}>
+              About Us
+            </Text>
+            <Icon
+              name="chevron-right"
+              size={20}
+              color={isDarkMode ? "#FFFFFF" : "#000000"}
             />
-          </View>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.otherContainers}>
-        <TouchableOpacity
-          style={styles.otherContainersAlign}
-          onPress={() => navigation.navigate("AboutUs")}
-        >
-          <Text style={[styles.OptionText, isDarkMode && styles.darkText]}>
-            About Us
-          </Text>
-          <Icon
-            name="chevron-right"
-            size={20}
-            color={isDarkMode ? "#FFFFFF" : "#000000"}
-          />
-        </TouchableOpacity>
-      </View>
-      <View style={styles.otherContainers}>
-        <TouchableOpacity
-          style={styles.otherContainersAlign}
-          onPress={() => navigation.navigate("PrivacyPolicy")}
-        >
-          <Text style={[styles.OptionText, isDarkMode && styles.darkText]}>
-            Privacy Policy
-          </Text>
-          <Icon
-            name="chevron-right"
-            size={20}
-            color={isDarkMode ? "#FFFFFF" : "#000000"}
-          />
-        </TouchableOpacity>
-      </View>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.otherContainers}>
+          <TouchableOpacity
+            style={styles.otherContainersAlign}
+            onPress={() => navigation.navigate("PrivacyPolicy")}
+          >
+            <Text style={[styles.OptionText, isDarkMode && styles.darkText]}>
+              Privacy Policy
+            </Text>
+            <Icon
+              name="chevron-right"
+              size={20}
+              color={isDarkMode ? "#FFFFFF" : "#000000"}
+            />
+          </TouchableOpacity>
+        </View>
 
-      <TouchableOpacity style={styles.bug}>
-        <Icon name="bug" size={15} color={isDarkMode ? "#FFFFFF" : "#000000"} />
-        <Text style={[styles.bugText, isDarkMode && styles.darkText]}>
-          Report a Bug
+        <TouchableOpacity style={styles.bug}>
+          <Icon
+            name="bug"
+            size={15}
+            color={isDarkMode ? "#FFFFFF" : "#000000"}
+          />
+          <Text style={[styles.bugText, isDarkMode && styles.darkText]}>
+            Report a Bug
+          </Text>
+        </TouchableOpacity>
+        <Text style={[styles.versionNum, isDarkMode && styles.darkText]}>
+          V 0.1
         </Text>
-      </TouchableOpacity>
-      <Text style={[styles.versionNum, isDarkMode && styles.darkText]}>
-        V 0.1
-      </Text>
-    </View>
+      </View>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 150,
+    paddingTop: 50,
     padding: 20,
     backgroundColor: "#ffffff",
   },
