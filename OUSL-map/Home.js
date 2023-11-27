964d81f30c5a7a38fa233dd26a74e8c8fa6d539d
Thizh <s22010229@ousl.lk>
2023-11-27 12:17:29 +0530

    modified:   App.js 	modified:   Header.js 	modified:   Home.js 	modified:   MapComponent.js 	modified:   MapScreen.js 	modified:   Search.js

--- OUSL-map/Home.js
+++ OUSL-map/Home.js
@@ -1,22 +1,30 @@
 import React from 'react';
-import { SafeAreaView, View, TouchableOpacity, Text, StatusBar } from 'react-native';
+import { SafeAreaView, View, Text, StatusBar } from 'react-native';
 import MapComponent from './MapComponent';
 import BottomToolbar from './BottomToolbar';
-import { useTheme } from "./ThemeContext";
+import { useTheme } from './ThemeContext';
 
-const Home = ({ navigation }) => {
+const Home = ({ route, navigation }) => {
   const { isDarkMode } = useTheme();
+  const selectedPlace = route.params?.selectedPlace;
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-      />
-      <MapComponent />
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+
+      {selectedPlace ? (
+        // Render MapComponent with the selected place if available
+        <MapComponent selectedPlace={selectedPlace} />
+      ) : (
+        // Render the default MapComponent without a selected place
+        <MapComponent />
+      )}
+
       <View style={{ bottom: 0, position: 'absolute', marginBottom: 120, marginLeft: 18 }}>
         <Text style={{ fontWeight: 500, fontSize: 14, color: isDarkMode ? '#FFFFFF' : '#000000' }}>UNIMATES</Text>
         <Text style={{ color: isDarkMode ? '#FFFFFF' : '#000000' }}>&copy; 2023</Text>
       </View>
+
       <BottomToolbar navigation={navigation} />
     </SafeAreaView>
   );
