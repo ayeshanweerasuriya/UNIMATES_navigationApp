d27c7870b4b6202409af9b15fb564f7884b04899
Melanie Sankalpana <uamsankalpana@gmail.com>
2023-11-30 14:19:04 +0530

    Update data.js
    I added some locations. check this..

--- OUSL-map/data.js
+++ OUSL-map/data.js
@@ -1,8 +1,20 @@
 // data.js
 export const placesArray = [
-    { name: 'Open University Bridge', coordinates: [6.888954, 79.881006] },
-    { name: 'Dormitory', coordinates: [6.887120, 79.881357] },
-    { name: 'Computer Laboratory', coordinates: [6.886715, 79.881615] },
+    { name: 'Open University Bridge', coordinates: [6.888954, 79.881006], description:'' },
+    { name: 'Dormitory', coordinates: [6.887120, 79.881357], description:'' },
+    { name: 'Computer Laboratory', coordinates: [6.886715, 79.881615], description:'' },
+    { name: 'CRC Office', coordinates: [6.88347498868237, 79.88655932522124],description:'' },
+    { name: 'Student Registration Office', coordinates: [6.883137551188019, 79.88628892959879], description:'' },
+    { name: 'Mechanical Engineering Workshop', coordinates: [6.8836393446842585, 79.88575176962465], description:'' },
+    { name: 'Industrial Automation Lab and Mechanical Engineering Labs', coordinates: [6.883413535843137, 79.8858385911815], description:'' },
+    { name: 'Lecture Halls', coordinates: [6.883224081108119, 79.88568984585179], description:'' },
+    { name: 'Block 19', coordinates: [6.883009809764903, 79.8855902334538], description:'' },
+    { name: 'BOC ATM', coordinates: [6.8828221635785924, 79.88587039648], description:'' },
+    { name: 'Block 7 Auditorium', coordinates: [6.883625915510159, 79.88528387853664], description:'' },
+    { name: 'Block 8 Lecture Hall', coordinates: [6.883409415177609, 79.88518457055308], description:'' },
+    { name: 'Block 9 Lecture Hall', coordinates: [6.883185831013677, 79.88515251846407], description:'' },
+    { name: 'Faculty of Health Sciences', coordinates: [6.882886352221996, 79.8853738495279], description:'' },
+    { name: 'Block 10 Lecture Halls', coordinates: [6.8829997643255005, 79.88496867087501], description:'' },
     // ... other places
   ];
   
