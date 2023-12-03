81a982766d83a10d814516a5d44eb098177c16bd
ayeshanweerasuriya <98686381+ayeshanweerasuriya@users.noreply.github.com.>
2023-12-03 23:24:24 +0530

    Implement Dark theme for BugReport.js

--- OUSL-map/BugReport.js
+++ OUSL-map/BugReport.js
@@ -10,8 +10,12 @@ import {
   Platform,
 } from "react-native";
 import { Entypo, FontAwesome } from "@expo/vector-icons";
+import { useTheme } from "./ThemeContext";
 
 const BugReport = ({ onClose }) => {
+  const { isDarkMode } = useTheme();
+  const styles = createStyles(isDarkMode);
+
   const handleDismissKeyboard = () => {
     Keyboard.dismiss();
   };
@@ -29,10 +33,12 @@ const BugReport = ({ onClose }) => {
         <View style={styles.popup}>
           <View style={styles.heading}>
             <TouchableOpacity onPress={onClose}>
-              <Entypo name="cross" size={24} color="black" />
+              <Entypo name="cross" size={24} style={styles.cross} />
             </TouchableOpacity>
             <Text style={styles.headingText}>Bug Report</Text>
-            <FontAwesome name="paper-plane" size={15} color="black" />
+            <TouchableOpacity>
+              <Entypo name="paper-plane" size={15} style={styles.paperPlane} />
+            </TouchableOpacity>
           </View>
           {/* Add a border below the title */}
           <View style={styles.titleBorder} />
@@ -49,7 +55,8 @@ const BugReport = ({ onClose }) => {
               placeholder="Description.. Please be as detailed as possible. What did you expect and what happened instead?"
               multiline={true}
               onSubmitEditing={handleDismissKeyboard}
-              maxLength={100}
+              placeholderTextColor="#888"
+              maxLength={300}
             />
           </View>
         </View>
@@ -58,58 +65,69 @@ const BugReport = ({ onClose }) => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  popupContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  popup: {
-    backgroundColor: "white",
-    padding: 20,
-    borderRadius: 10,
-    width: "80%",
-  },
-  heading: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  headingText: {
-    fontSize: 18,
-    fontWeight: "bold",
-    color: "black",
-  },
-  titleBorder: {
-    borderBottomWidth: 1,
-    borderBottomColor: "black",
-    width: "100%",
-    marginVertical: 10,
-  },
-  userEmail: {
-    padding: 20,
-    borderRadius: 20,
-    marginVertical: 10,
-    width: 250, // Set an absolute width in pixels
-    backgroundColor: "#f2f2f2",
-    textAlign: "center", // Center the text
-  },
+const createStyles = (isDarkMode) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    popupContainer: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    popup: {
+      backgroundColor: isDarkMode ? "#1E1E1E" : "#ffffff",
+      padding: 20,
+      borderRadius: 10,
+      width: "80%",
+    },
+    heading: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+    },
+    headingText: {
+      fontSize: 18,
+      fontWeight: "bold",
+      color: isDarkMode ? "#FFFFFF" : "#343434",
+    },
+    titleBorder: {
+      borderBottomWidth: 1,
+      borderBottomColor: "black",
+      width: "100%",
+      marginVertical: 10,
+    },
+    userEmail: {
+      padding: 20,
+      borderRadius: 20,
+      marginVertical: 10,
+      width: 250, // Set an absolute width in pixels
+      backgroundColor: isDarkMode ? "#2e2e2e" : "#2e2e2e",
+      textAlign: "center", // Center the text
+      color: isDarkMode ? "#ffffff" : "#1E1E1E",
+    },
 
-  description: {
-    padding: 20,
-    paddingTop: 15,
-    borderRadius: 20,
-    backgroundColor: "#f2f2f2",
-    minHeight: 100,
-    maxHeight: 200,
-    textAlignVertical: "top",
-    width: 250, // Set an absolute width in pixels
-  },
-});
+    description: {
+      padding: 20,
+      paddingTop: 15,
+      borderRadius: 20,
+      backgroundColor: isDarkMode ? "#2e2e2e" : "#2e2e2e",
+      minHeight: 100,
+      maxHeight: 200,
+      textAlignVertical: "top",
+      width: 250, // Set an absolute width in pixels
+      color: isDarkMode ? "#ffffff" : "#1E1E1E",
+    },
+
+    cross: {
+      color: isDarkMode ? "#FFFFFF" : "#343434",
+    },
+
+    paperPlane: {
+      color: isDarkMode ? "#FFFFFF" : "#343434",
+    },
+  });
 
 export default BugReport;
