1f8e26765cfdb2160e2528028bfc59a0e6601977
ayeshanweerasuriya <98686381+ayeshanweerasuriya@users.noreply.github.com.>
2023-12-04 11:20:27 +0530

    well structured code BugReport.js

--- OUSL-map/BugReport.js
+++ OUSL-map/BugReport.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
   View,
@@ -21,9 +21,17 @@ const BugReport = ({ onClose }) => {
   const [userEmail, setUserEmail] = useState("");
   const [showThankYou, setShowThankYou] = useState(false);
 
-  const handleDismissKeyboard = () => {
-    Keyboard.dismiss();
-  };
+  useEffect(() => {
+    if (showThankYou) {
+      const closeBugReport = setTimeout(() => {
+        setShowThankYou(false);
+        onClose();
+      }, 2000);
+
+      return () => clearTimeout(closeBugReport);
+    }
+    return () => {};
+  }, [showThankYou, onClose]);
 
   const handleSendEmail = async () => {
     const emailSubject = "Bug Report";
@@ -40,12 +48,6 @@ const BugReport = ({ onClose }) => {
 
         // Show the "thank you" message
         setShowThankYou(true);
-
-        // Close the BugReport component after 2 seconds
-        setTimeout(() => {
-          setShowThankYou(false);
-          onClose();
-        }, 2000);
       } else {
         console.warn("Mail Composer is not available on this device");
       }
@@ -58,7 +60,7 @@ const BugReport = ({ onClose }) => {
     <TouchableOpacity
       style={styles.container}
       activeOpacity={1}
-      onPress={handleDismissKeyboard}
+      onPress={() => Keyboard.dismiss()}
     >
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
@@ -89,21 +91,23 @@ const BugReport = ({ onClose }) => {
               <View style={styles.titleBorder} />
               <View>
                 <TextInput
-                  style={styles.userEmail}
+                  style={styles.input}
                   placeholder="Your email"
                   value={userEmail}
                   onChangeText={(text) => setUserEmail(text)}
-                  onSubmitEditing={handleDismissKeyboard}
+                  onSubmitEditing={() => Keyboard.dismiss()}
                   placeholderTextColor="#888"
                   maxLength={35}
+                  keyboardType="email-address"
+                  autoCapitalize="none"
                 />
                 <TextInput
-                  style={styles.description}
+                  style={[styles.input, styles.description]}
                   placeholder="Description.. Please be as detailed as possible. What did you expect and what happened instead?"
                   multiline={true}
                   value={description}
                   onChangeText={(text) => setDescription(text)}
-                  onSubmitEditing={handleDismissKeyboard}
+                  onSubmitEditing={() => Keyboard.dismiss()}
                   placeholderTextColor="#888"
                   maxLength={300}
                 />
@@ -150,25 +154,20 @@ const createStyles = (isDarkMode) =>
       width: "100%",
       marginVertical: 10,
     },
-    userEmail: {
+    input: {
       padding: 20,
       borderRadius: 20,
       marginVertical: 10,
       width: 250,
-      backgroundColor: isDarkMode ? "#2e2e2e" : "#f2f2f2",
       textAlign: "center",
       color: isDarkMode ? "#ffffff" : "#1E1E1E",
+      backgroundColor: isDarkMode ? "#2e2e2e" : "#f2f2f2",
     },
     description: {
-      padding: 20,
       paddingTop: 15,
-      borderRadius: 20,
-      backgroundColor: isDarkMode ? "#2e2e2e" : "#f2f2f2",
       minHeight: 100,
       maxHeight: 200,
-      textAlignVertical: "top",
-      width: 250,
-      color: isDarkMode ? "#ffffff" : "#1E1E1E",
+      textAlign: "left",
     },
     cross: {
       color: isDarkMode ? "#FFFFFF" : "#343434",
