cded8c2c6ebf7b0565c2459c3256fa181d900a56
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2023-12-06 11:56:38 +0530

    Resolved merge Conflict

--- OUSL-map/App.js
+++ OUSL-map/App.js
@@ -9,10 +9,7 @@ import Settings from "./Screens/Settings";
 import PrivacyPolicy from "./Screens/Privacy_Policy";
 import AboutUs from "./Screens/AboutUs";
 import MapComponent from "./Screens/MapComponent";
-<<<<<<< HEAD
-import BugReport from "./Screens/BugReport";
-=======
->>>>>>> 58405e4dac8315df5f67319f70c0de3b72b7b7f2
+import BugReport from "./Screens/BugReport"; // Keep this line or the one below based on your preference
 
 const Stack = createStackNavigator();
 
@@ -20,57 +17,18 @@ const AppNavigator = () => {
   return (
     <ThemeProvider>
       <NavigationContainer>
-<<<<<<< HEAD
-        <Stack.Navigator initialRouteName="LaunchScreen">
-          <Stack.Screen
-            name="LaunchScreen"
-            component={LaunchScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="SearchBar"
-            component={SearchBar}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="MapComponent"
-            component={MapComponent}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="Settings"
-            component={Settings}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="PrivacyPolicy"
-            component={PrivacyPolicy}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="AboutUs"
-            component={AboutUs}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="BugReport"
-            component={BugReport}
-            options={{ headerShown: false }}
-          />
-=======
-        <Stack.Navigator initialRouteName="LaunchScreen" screenOptions={{ headerShown: false }}>
+        <Stack.Navigator
+          initialRouteName="LaunchScreen"
+          screenOptions={{ headerShown: false }}
+        >
           <Stack.Screen name="LaunchScreen" component={LaunchScreen} />
           <Stack.Screen name="Home" component={Home} />
           <Stack.Screen name="MapComponent" component={MapComponent} />
           <Stack.Screen name="Settings" component={Settings} />
           <Stack.Screen name="PrivacyPolicy" component={PrivacyPolicy} />
           <Stack.Screen name="AboutUs" component={AboutUs} />
->>>>>>> 58405e4dac8315df5f67319f70c0de3b72b7b7f2
+          <Stack.Screen name="BugReport" component={BugReport} />{" "}
+          {/* Keep this line or the one above based on your preference */}
         </Stack.Navigator>
       </NavigationContainer>
     </ThemeProvider>
